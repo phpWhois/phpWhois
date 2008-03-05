12c306d9a621c944a303564532c71d7714f04ca2
sparc <>
2008-03-05 18:45:53 +0000

    updated

--- whois.cz.php
+++ whois.cz.php
@@ -40,7 +40,7 @@ class cz_handler
                       'expire' 	=> 'expires',
                       'nserver' => 'nserver',
                       'domain' 	=> 'name',
-                      'nic-hdl' => 'handle',
+                      'contact' => 'handle',
                       'reg-c'	=> '',
                       'descr'	=> 'desc',
                       'e-mail'	=> 'email',
@@ -52,7 +52,8 @@ class cz_handler
 		$contacts = array(
                       'admin-c' => 'admin',
                       'tech-c' => 'tech',
-                      'bill-c' => 'billing'
+                      'bill-c' => 'billing',
+                      'registrant' => 'owner'
 		                  );
 
 		$r['regyinfo'] = array(
@@ -60,89 +61,7 @@ class cz_handler
                           'registrar' => 'CZ-NIC'
                           );
 
-		$blocks = generic_parser_a_blocks($data_str['rawdata'], $translate, $disclaimer);
-
-		if (isset($disclaimer) && is_array($disclaimer)) 
-			$reg['disclaimer'] = $disclaimer;
-
-		if (!isset($blocks) || !is_array($blocks['main']))
-			{
-			$reg['registered'] = 'no';
-			$r['regrinfo'] = $reg;
-			return ($r);
-			}
-		
-		$reg['registered'] = 'yes';
-
-		$reg['domain'] = $blocks['main'];	
-		$reg['owner'] = $blocks[$blocks['main']['admin-c']];	
-		
-		if (isset($reg['owner']['admin-c']))
-			{
-			if (is_array($reg['owner']['admin-c']))
-				$reg['admin'] = $blocks[$reg['owner']['admin-c'][0]];
-			else
-				$reg['admin'] = $blocks[$reg['owner']['admin-c']];
-			}
-		else
-			$reg['admin'] = $reg['owner'];
-			
-		if (isset($blocks['main']['tech-c']))
-			$tech = $blocks['main']['tech-c'];	
-		else
-			if (isset($reg['owner']['tech-c']))
-				$tech = $reg['owner']['tech-c'];					
-			else
-				$tech = '';
-
-		if ($tech != '')
-			{
-			if (is_array($tech))
-				$reg['tech'] = $blocks[$tech[0]];
-			else
-				$reg['tech'] = $blocks[$tech];
-			}
-			
-		if (isset($blocks['main']['bill-c']))
-			$bill = $blocks['main']['bill-c'];	
-		else
-			if (isset($reg['owner']['bill-c']))
-				$bill= $reg['owner']['bill-c'];					
-			else
-				$bill = '';
-
-		if ($bill != '')
-			{
-			if (is_array($bill))
-				$reg['bill'] = $blocks[$bill[0]];
-			else
-				$reg['bill'] = $blocks[$bill];
-			}
-			
-		if (isset($reg['domain']['tech-c'])) unset($reg['domain']['tech-c']);
-		if (isset($reg['domain']['admin-c'])) unset($reg['domain']['admin-c']);
-		if (isset($reg['domain']['bill-c'])) unset($reg['domain']['bill-c']);
-		
-		if (isset($reg['owner']['tech-c'])) unset($reg['owner']['tech-c']);
-		if (isset($reg['owner']['admin-c'])) unset($reg['owner']['admin-c']);
-		if (isset($reg['owner']['bill-c'])) unset($reg['owner']['bill-c']);
-		
-		if (isset($reg['admin']['tech-c'])) unset($reg['admin']['tech-c']);
-		if (isset($reg['admin']['admin-c'])) unset($reg['admin']['admin-c']);
-		if (isset($reg['admin']['bill-c'])) unset($reg['admin']['bill-c']);
-
-		if (isset($reg['tech']['tech-c'])) unset($reg['tech']['tech-c']);
-		if (isset($reg['tech']['admin-c'])) unset($reg['tech']['admin-c']);
-		if (isset($reg['tech']['bill-c'])) unset($reg['tech']['bill-c']);
-		
-		if (isset($reg['bill']))
-			{
-			if (isset($reg['bill']['tech-c'])) unset($reg['bill']['tech-c']);
-			if (isset($reg['bill']['admin-c'])) unset($reg['bill']['admin-c']);
-			if (isset($reg['bill']['bill-c'])) unset($reg['bill']['bill-c']);
-			}
-
-		format_dates($reg,'Ymd');
+		$reg = generic_parser_a($data_str['rawdata'], $translate, $contacts, 'domain', 'dmy');
 		$r['regrinfo'] = $reg;
 		return ($r);
 		}
