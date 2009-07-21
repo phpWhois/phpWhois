659d78cf32242b2a54c6c7f0aa36bce244aa026a
sparc <>
2009-07-21 09:10:32 +0000

    removed support for .es

--- whois.servers.php
+++ whois.servers.php
@@ -67,7 +67,7 @@ $this->WHOIS_SPECIAL = array(
 		'by'	 => '',
 		'bz'	 => 'whois2.afilias-grs.net',		
 		'cy'	 => '',
-		'es'	 => 'http://shop.arsys.es/whois.php?dominio={domain}.es',
+		'es'	 => '',
 		'fm'     => 'http://www.dot.fm/query_whois.cfm?domain={domain}&tld=fm',		
 		'jobs'	 => 'jobswhois.verisign-grs.com',				
 		'la'	 => 'whois.centralnic.net',		
