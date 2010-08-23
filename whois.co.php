859b6e3d2507cf5b0107ff6b2ef2298626f3181b
sparc <>
2010-08-23 11:46:50 +0000

    added .co handler

---
+++ whois.co.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Whois.php        PHP classes to conduct whois queries
+
+Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+
+Maintained by David Saez (david@ols.es)
+
+For the most recent version of this package visit:
+
+http://www.phpwhois.org
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+if (!defined('__CO_HANDLER__'))
+	define('__CO_HANDLER__', 1);
+
+require_once('whois.parser.php');
+
+class co_handler
+	{
+	function parse($data_str, $query)
+		{
+		$r['regrinfo'] = generic_parser_b($data_str['rawdata']);
+		$r['regyinfo']['referrer'] = 'http://www.cointernet.com.co/';
+		$r['regyinfo']['registrar'] = '.CO Internet, S.A.S.';
+		return ($r);
+		}
+	}
+?>
