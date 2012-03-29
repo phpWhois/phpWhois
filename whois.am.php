65edba1c0673b17759019d70c46cb748aa3a8e97
sparc <>
2012-03-29 10:41:42 +0000

    added am handler

---
+++ whois.am.php
@@ -0,0 +1,42 @@
+<?php
+if (!defined('__AM_HANDLER__'))
+	define('__AM_HANDLER__', 1);
+
+require_once('whois.parser.php');
+
+class am_handler
+	{
+	function parse($data_str, $query)
+		{
+		$items = array(
+                'owner' 			=> 'Registrant:',
+                'domain.name' 		=> 'Domain name:',
+                'domain.created' 	=> 'Registered:',
+                'domain.changed' 	=> 'Last modified:',
+                'domain.nserver' 	=> 'DNS servers:',
+                'domain.status'		=> 'Status:',
+                'tech' 				=> 'Technical contact:',
+                'admin'				=> 'Administrative contact:',
+		            );
+
+		$r['regrinfo'] = get_blocks($data_str['rawdata'], $items);
+
+		if (!empty($r['regrinfo']['domain']['name']))
+			{
+			$r['regrinfo'] = get_contacts($r['regrinfo'],$trans);
+			$r['regrinfo']['registered'] = 'yes';
+			}
+		else
+			{
+			$r = '';
+			$r['regrinfo']['registered'] = 'no';
+			}
+
+		$r['regyinfo'] = array(
+                    'referrer' => 'http://www.isoc.am',
+                    'registrar' => 'ISOCAM'
+                    );
+
+		return $r;
+		}
+	}
