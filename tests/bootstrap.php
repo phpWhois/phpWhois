db14361ff04ead8ed22ddf8383c3f0e2e3e7dab3
Dmitry Lukashin <dmitry@lukashin.ru>
2014-12-26 04:04:16 +0300

    Update testing environment in more mature matter

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
-$whoisclass = __DIR__.'/../src/whois.main.php';
-require_once($whoisclass);
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require(__DIR__ . '/../vendor/autoload.php');
+}
