5f672c6a1c8d81131b4dc0c3f24220fe77e4912b
Adam Jimenez <adam.jimenez@gmail.com>
2018-01-18 10:38:38 +0000

    whois not parsed correctly e.g google.co.uk
    This is a more php7 friendly version of #3
    It doesn't use list or each.

--- src/whois.parser.php
+++ src/whois.parser.php
@@ -396,7 +396,9 @@ function get_blocks($rawdata, $items, $partial_match = false, $def_block = false
     $r = array();
     $endtag = '';
 
-    foreach ($rawdata as $key => $val) {
+    while ($val = current($rawdata)) {
+        next($rawdata);
+        
         $val = trim($val);
         if ($val == '')
             continue;
@@ -440,7 +442,9 @@ function get_blocks($rawdata, $items, $partial_match = false, $def_block = false
 
         // Block found, get data ...
 
-        foreach ($rawdata as $val) {
+        while ($val = current($rawdata)) {
+            next($rawdata);
+            
             $val = trim($val);
 
             if ($val == '' || $val == str_repeat($val[0], strlen($val)))
