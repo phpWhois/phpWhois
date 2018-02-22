045d601da2ed58ccc3f32d2571ee26c1d8150961
develike <36663024+develike@users.noreply.github.com>
2018-02-22 10:38:57 +0300

    Notice: Undefined offset: 1
    You have had this in previous versions (without error)

--- src/whois.ip.php
+++ src/whois.ip.php
@@ -95,7 +95,9 @@ class ip_handler extends WhoisClient {
 
                 if ($p !== false) {
                     $net = strtok(substr($line, $p + 1), ') ');
-                    list($low, $high) = explode('-', str_replace(' ', '', substr($line, $p + strlen($net) + 3)));
+                    $arr = explode('-',str_replace(' ','',substr($line,$p+strlen($net)+3)));
+                    $low = $arr[0];
+                    $high = !empty($arr[1])?$arr[1]:'';
 
                     if (!isset($done[$net]) && $ip >= ip2long($low) && $ip <= ip2long($high)) {
                         $owner = substr($line, 0, $p - 1);
