e4fa2c7115ce41ff646e6e5060082cc3eb837a6d
Philippe Meunier <pmeunier@cybergeneration.com>
2019-08-08 15:30:08 -0400

    - Fix .ca domain parsing, CIRA changed its format in Feb 2019

--- src/whois.ca.php
+++ src/whois.ca.php
@@ -30,37 +30,8 @@ require_once('whois.parser.php');
 class ca_handler {
 
     function parse($data_str, $query) {
-        $items = array(
-            'owner' => 'Registrant:',
-            'admin' => 'Administrative contact:',
-            'tech' => 'Technical contact:',
-            'domain.sponsor' => 'Registrar:',
-            'domain.nserver' => 'Name servers:',
-            'domain.status' => 'Domain status:',
-            'domain.created' => 'Creation date:',
-            'domain.expires' => 'Expiry date:',
-            'domain.changed' => 'Updated date:'
-        );
-
-        $extra = array(
-            'postal address:' => 'address.0',
-            'job title:' => '',
-            'number:' => 'handle',
-            'description:' => 'organization'
-        );
-
         $r = array();
-        $r['regrinfo'] = easy_parser($data_str['rawdata'], $items, 'ymd', $extra);
-
-        if (!empty($r['regrinfo']['domain']['sponsor'])) {
-            list($v, $reg) = explode(':', $r['regrinfo']['domain']['sponsor'][0]);
-            $r['regrinfo']['domain']['sponsor'] = trim($reg);
-        }
-
-        if (empty($r['regrinfo']['domain']['status']) || $r['regrinfo']['domain']['status'] == 'available')
-            $r['regrinfo']['registered'] = 'no';
-        else
-            $r['regrinfo']['registered'] = 'yes';
+        $r['regrinfo'] = generic_parser_b($data_str['rawdata'], [], 'ymd');
 
         $r['regyinfo'] = array(
             'registrar' => 'CIRA',
@@ -68,5 +39,4 @@ class ca_handler {
         );
         return $r;
     }
-
 }
