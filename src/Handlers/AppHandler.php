54d31aee94ee46db022ae6bc90a80358c6ee8175
Joshua Smith <jsmith@jssm.com>
2020-06-08 18:47:25 -0400

    Refactor app_handler

--- src/Handlers/AppHandler.php
+++ src/Handlers/AppHandler.php
@@ -1,42 +1,17 @@
 <?php
 /**
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2
- * @license
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * @link      http://phpwhois.pw
- * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
- * @copyright Maintained by David Saez
- * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @license   See LICENSE file
+ * @copyright Copyright (c) 2020 Joshua Smith
  */
 
-if (!defined('__APP_HANDLER__')) {
-    define('__APP_HANDLER__', 1);
-}
+namespace phpWhois\Handlers;
 
-require_once('whois.parser.php');
-
-/**
- * Class app_handler
- */
-class app_handler
+class AppHandler extends AbstractHandler
 {
-    function parse($data_str, $query)
+    public function parse(array $data_str, string $query): array
     {
         $r = [
-            'regrinfo' => generic_parser_b($data_str['rawdata']),
+            'regrinfo' => $this->generic_parser_b($data_str['rawdata']),
             'rawdata'  => $data_str['rawdata'],
         ];
 
--- tests/Handlers/AppHandlerTest.php
+++ tests/Handlers/AppHandlerTest.php
@@ -1,22 +1,7 @@
 <?php
 /**
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2
- * @license
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * @copyright Copyright (c) 2018 Joshua Smith
+ * @license   See LICENSE file
+ * @copyright Copyright (c) 2020 Joshua Smith
  */
 
 namespace phpWhois\Handlers;
@@ -27,7 +12,7 @@ namespace phpWhois\Handlers;
 class AppHandlerTest extends HandlerTest
 {
     /**
-     * @var \app_handler $handler
+     * @var AppHandler $handler
      */
     protected $handler;
 
@@ -38,7 +23,7 @@ class AppHandlerTest extends HandlerTest
     {
         parent::setUp();
 
-        $this->handler            = new \app_handler();
+        $this->handler            = new AppHandler();
         $this->handler->deepWhois = false;
     }
 
