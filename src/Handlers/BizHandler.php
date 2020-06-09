79c57afe7f409173da54060b3d1f94c787d1e283
Joshua Smith <jsmith@jssm.com>
2020-06-08 20:57:33 -0400

    Refactor biz_handler

--- src/Handlers/BizHandler.php
+++ src/Handlers/BizHandler.php
@@ -1,42 +1,27 @@
 <?php
 /**
- * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2
- * @license
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
- * @link http://phpwhois.pw
+ * @license   See LICENSE file
  * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
  * @copyright Maintained by David Saez
  * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2020 Joshua Smith
  */
 
-if (!defined('__BIZ_HANDLER__'))
-    define('__BIZ_HANDLER__', 1);
+namespace phpWhois\Handlers;
 
-require_once('whois.parser.php');
+class BizHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $r = [
+            'regrinfo' => $this->generic_parser_b($data_str['rawdata'], [], 'mdy'),
+            'regyinfo' => [
+                'referrer'  => 'http://www.neulevel.biz',
+                'registrar' => 'NEULEVEL',
+            ],
+            'rawdata'  => $data_str['rawdata'],
+        ];
 
-class biz_handler {
-
-    function parse($data_str, $query) {
-        $r = array();
-        $r['regrinfo'] = generic_parser_b($data_str['rawdata'], array(), 'mdy');
-        $r['regyinfo'] = array(
-            'referrer' => 'http://www.neulevel.biz',
-            'registrar' => 'NEULEVEL'
-        );
         return $r;
     }
-
 }
---
+++ tests/Handlers/BizHandlerTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @copyright Copyright (c) 2020 Joshua Smith
+ * @license   See LICENSE file
+ */
+
+namespace phpWhois\Handlers;
+
+/**
+ * BizHandlerTest
+ */
+class BizHandlerTest extends HandlerTest
+{
+    /**
+     * @var BizHandler $handler
+     */
+    protected $handler;
+
+    /**
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->handler            = new BizHandler();
+        $this->handler->deepWhois = false;
+    }
+
+    /**
+     * @return void
+     *
+     * @test
+     */
+    public function parseNeulevelDotBiz()
+    {
+        $query = 'neulevel.biz';
+
+        $fixture = $this->loadFixture($query);
+        $data    = [
+            'rawdata'  => $fixture,
+            'regyinfo' => [],
+        ];
+
+        $actual = $this->handler->parse($data, $query);
+
+        $expected = [
+            'domain'     => [
+                'name'    => 'neulevel.biz',
+                'changed' => '2020-03-05',
+                'created' => '2001-09-30',
+                'expires' => '2020-11-06',
+            ],
+            'registered' => 'yes',
+        ];
+
+        $this->assertArraySubset($expected, $actual['regrinfo'], 'Whois data may have changed');
+        $this->assertArrayHasKey('rawdata', $actual);
+        $this->assertArraySubset($fixture, $actual['rawdata'], 'Fixture data may be out of date');
+    }
+}
