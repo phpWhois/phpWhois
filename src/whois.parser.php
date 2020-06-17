2d200d718941b4dfb12b15de05f30ce4f054b797
Joshua Smith <jsmith@jssm.com>
2020-06-16 22:20:02 -0400

    Work around date-parsing bug fixed in PHP 7.3 `v` format character for milliseconds was broken before PHP 7.3

--- src/whois.parser.php
+++ src/whois.parser.php
@@ -915,20 +915,28 @@ function get_date($date, $format)
  *
  * @return false|DateTime
  */
-function parseStandardDate(string $date) {
+function parseStandardDate(string $date)
+{
     $date = trim($date);
+
+    // 2020-01-01T00:00:00.0Z
+    $pattern = '/^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d(Z)$/';
+    if (preg_match($pattern, $date, $matches)) {
+        if (PHP_VERSION_ID >= 70300) {
+            // The `v` format character is broken before PHP 7.3
+            $dateTimeFormat = 'Y-m-d\TH:i:s.vT';
+            return Datetime::createFromFormat($dateTimeFormat, $date);
+        }
+
+        // For PHP < 7.3, skip the milliseconds
+        $date = $matches[1] . $matches[2];
+        // Intentional fall-through--this will match the next pattern
+    }
 
     // 2020-01-01T00:00:00Z
     $pattern = '/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/';
     if (preg_match($pattern, $date)) {
         $dateTimeFormat = 'Y-m-d\TH:i:sT';
-        return Datetime::createFromFormat($dateTimeFormat, $date);
-    }
-
-    // 2020-01-01T00:00:00.0Z
-    $pattern = '/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\dZ$/';
-    if (preg_match($pattern, $date)) {
-        $dateTimeFormat = 'Y-m-d\TH:i:s.vT';
         return Datetime::createFromFormat($dateTimeFormat, $date);
     }
 
