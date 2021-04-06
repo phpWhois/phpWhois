e4bf1f3862f7e6ae7c6e8e22b08094c087f9c7ca
Adam Jimenez <adam.jimenez@gmail.com>
2021-04-06 10:16:44 +0100

    fix expiry date

--- src/whois.co.php
+++ src/whois.co.php
@@ -31,7 +31,7 @@ class co_handler {
 
     function parse($data_str, $query) {
         $r = array();
-        $r['regrinfo'] = generic_parser_b($data_str['rawdata'], array(), '-md--y');
+        $r['regrinfo'] = generic_parser_b($data_str['rawdata'], array(), 'ymd');
         $r['regyinfo']['referrer'] = 'http://www.cointernet.com.co/';
         $r['regyinfo']['registrar'] = '.CO Internet, S.A.S.';
         return $r;
