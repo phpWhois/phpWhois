077127c027580c55c78e9ef066d4f011bc4654b4
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:56:27 +0200

    Use LyHandler instead whois.ly.php

---
+++ src/Handlers/LyHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class LyHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $items = array(
+            'owner' => 'Registrant:',
+            'admin' => 'Administrative Contact:',
+            'tech' => 'Technical Contact:',
+            'domain.name' => 'Domain Name:',
+            'domain.status' => 'Domain Status:',
+            'domain.created' => 'Created:',
+            'domain.changed' => 'Updated:',
+            'domain.expires' => 'Expired:',
+            'domain.nserver' => 'Domain servers in listed order:'
+        );
+
+        $extra = array('zip/postal code:' => 'address.pcode');
+
+        $r = [
+            'regrinfo' => static::getBlocks($data_str['rawdata'], $items),
+            'regyinfo' => [
+                'referrer' => 'https://www.nic.ly',
+                'registrar' => 'Libya ccTLD'
+            ],
+        ];
+
+        if (!empty($r['regrinfo']['domain']['name'])) {
+            $r['regrinfo'] = static::getContacts($r['regrinfo'], $extra);
+            $r['regrinfo']['domain']['name'] = $r['regrinfo']['domain']['name'][0];
+            $r['regrinfo']['registered'] = 'yes';
+        } else {
+            $r = array('regrinfo' => array());
+            $r['regrinfo']['registered'] = 'no';
+        }
+
+        return $r;
+    }
+}
