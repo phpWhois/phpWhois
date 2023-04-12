c2e0313a0774562ea7848013b9c80cd00843c78e
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:55:59 +0200

    Thanks to @jsmitty12

---
+++ src/Handlers/LondonHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class LondonHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $r = [
+            'regrinfo' => static::generic_parser_b($data_str['rawdata']),
+            'rawdata'  => $data_str['rawdata'],
+        ];
+
+        if (!isset($r['regrinfo']['domain']['name'])) {
+            $r['regrinfo']['domain']['name'] = $query;
+        }
+
+        return $r;
+    }
+}
