0c83a7e647dd36c0a7c5b1a085c503eba5b489e4
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:57:10 +0200

    Use MobiHandler instead whois.mobi.php

---
+++ src/Handlers/MobiHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class MobiHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $r = array();
+        $r['regrinfo'] = static::generic_parser_b($data_str['rawdata']);
+
+        if (!strncmp($data_str['rawdata'][0], 'WHOIS LIMIT EXCEEDED', 20)) {
+            $r['regrinfo']['registered'] = 'unknown';
+        }
+
+        $r['regyinfo']['referrer'] = 'http://www.mtld.mobi/';
+        $r['regyinfo']['registrar'] = 'Dot Mobi Registry';
+        return $r;
+    }
+}
