22df5177e4cab6e3d8df0816f1e3fb3940ddf05f
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:55:25 +0200

    Use IsHandler instead whois.is.php

---
+++ src/Handlers/IsHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class IsHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $translate = array(
+            'fax-no' => 'fax',
+            'e-mail' => 'email',
+            'nic-hdl' => 'handle',
+            'person' => 'name'
+        );
+
+        $contacts = array(
+            'owner-c' => 'owner',
+            'admin-c' => 'admin',
+            'tech-c' => 'tech',
+            'billing-c' => 'billing',
+            'zone-c' => 'zone'
+        );
+
+        $reg = static::generic_parser_a($data_str['rawdata'], $translate, $contacts, 'domain', 'mdy');
+
+        if (isset($reg['domain']['descr'])) {
+            $reg['owner']['name'] = array_shift($reg['domain']['descr']);
+            $reg['owner']['address'] = $reg['domain']['descr'];
+            unset($reg['domain']['descr']);
+        }
+
+        $r = array();
+        $r['regrinfo'] = $reg;
+        $r['regyinfo'] = array(
+            'referrer' => 'http://www.isnic.is',
+            'registrar' => 'ISNIC'
+        );
+        return $r;
+    }
+}
