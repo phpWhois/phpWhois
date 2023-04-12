bfc2ca587652b02212614c4a8d052e9222b52544
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:51:00 +0200

    Use CatHandler instead whois.cat.php

---
+++ src/Handlers/CatHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class CatHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $r = [
+            'regrinfo' => $this->generic_parser_b($data_str['rawdata']),
+            'regyinfo' => [
+                'registrar' => 'Domini punt CAT',
+                'referrer' => 'https://domini.cat/'
+            ],
+            'rawdata' => $data_str['rawdata'],
+        ];
+
+        if (!isset($r['regrinfo']['domain']['name'])) {
+            $r['regrinfo']['registered'] = 'no';
+        }
+
+        return $r;
+    }
+}
