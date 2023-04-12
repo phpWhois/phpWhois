f707bd8078dc4fdd2f7dc445af9113920412ff75
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:52:44 +0200

    Thanks to @jsmitty12

---
+++ src/Handlers/DevHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class DevHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $r = [
+            'regrinfo' => generic_parser_b($data_str['rawdata']),
+            'rawdata'  => $data_str['rawdata'],
+        ];
+
+        return $r;
+    }
+}
