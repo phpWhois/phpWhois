f4490477d8121bf9847999e5a8831fa80446afa7
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:50:54 +0200

    Use CaHandler instead whois.ca.php

---
+++ src/Handlers/CaHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class CaHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        return [
+            'regrinfo' => static::generic_parser_b($data_str['rawdata'], [], 'ymd'),
+            'regyinfo' => [
+                'registrar' => 'CIRA',
+                'referrer' => 'https://www.cira.ca/'
+            ],
+            'rawdata' => $data_str['rawdata'],
+        ];
+    }
+}
