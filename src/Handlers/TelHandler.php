160dc0a59ca5549abab233f1cd0bba22683cda4f
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 12:00:46 +0200

    Use TelHandler instead whois.tel.php

---
+++ src/Handlers/TelHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class TelHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $r = array();
+        $r['regrinfo'] = static::generic_parser_b($data_str['rawdata'], array(), 'mdy');
+        $r['regyinfo'] = array(
+            'referrer' => 'https://www.telnic.org',
+            'registrar' => 'Telnic'
+        );
+        return $r;
+    }
+}
