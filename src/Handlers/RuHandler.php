8afd0887d47b934ead51ed5c382953523f913202
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:59:51 +0200

    Use RuHandler instead whois.ru.php

---
+++ src/Handlers/RuHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2023 Kevin Lucich
+ */
+
+namespace phpWhois\Handlers;
+
+
+class RuHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        $items = [
+            'domain:'    => 'domain.name',
+            'registrar:' => 'domain.sponsor',
+            'state:'     => 'domain.status',
+            'nserver:'   => 'domain.nserver.',
+            'source:'    => 'domain.source',
+            'created:'   => 'domain.created',
+            'paid-till:' => 'domain.expires',
+            'type:'      => 'owner.type',
+            'org:'       => 'owner.organization',
+            'phone:'     => 'owner.phone',
+            'fax-no:'    => 'owner.fax',
+            'e-mail:'    => 'owner.email',
+        ];
+
+        $r = [
+            'rawdata' => $data_str['rawdata'],
+        ];
+
+        $r['regrinfo'] = generic_parser_b($data_str['rawdata'], $items, 'dmy');
+
+        if (empty($r['regrinfo']['domain']['status'])) {
+            $r['regrinfo']['registered'] = 'no';
+        }
+
+        $r['regyinfo'] = [
+            'referrer'  => 'http://www.ripn.net',
+            'registrar' => 'RU-CENTER-REG-RIPN',
+        ];
+
+        return $r;
+    }
+}
