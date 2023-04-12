3d42d30897ee47d1e7b1e12c19b0cb3b036080a7
Kevin Lucich <klucich@griffeshield.com>
2023-04-12 11:48:27 +0200

    Use AgHandler instead whois.ag.php

---
+++ src/Handlers/AgHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license   See LICENSE file
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2020 Joshua Smith
+ */
+
+namespace phpWhois\Handlers;
+
+class AgHandler extends AbstractHandler
+{
+    public function parse(array $data_str, string $query): array
+    {
+        return [
+            'regrinfo' => static::generic_parser_b($data_str['rawdata']),
+            'regyinfo' => [
+                'referrer'  => 'https://www.nic.ag',
+                'registrar' => 'Nic AG',
+            ],
+            'rawdata'  => $data_str['rawdata'],
+        ];
+    }
+}
